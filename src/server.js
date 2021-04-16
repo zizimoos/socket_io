fc4cc1aa9af495aeb9705a31e3df6f06883398b1
zizimoos_iMac <zizimoos@gmail.com>
2021-04-17 01:09:55 +0900

    setup

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 import express from "express"
+import socketIO from "socket.io"
+import { join } from "path"
+
 
 const app = express()
+app.set("view engine", "pug")
+app.set("views", join(__dirname,"views"))
+app.use(express.static(join(__dirname,"static")))
+app.get("/", (req,res) => res.render("home"))
+const server = app.listen(4000, () => console.log(`✅ server connected : http://localhost:4000 `))
 
-app.listen(4000, () => console.log(`✅ server connected : http://localhost:4000 `))
+const io = socketIO(server)
