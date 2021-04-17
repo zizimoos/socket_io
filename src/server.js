4f1b88ed1c742f04dfc4c5191f8e54c465cfa668
zizimoos_iMac <zizimoos@gmail.com>
2021-04-17 17:57:18 +0900

    ...

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 import express from "express"
 import socketIO from "socket.io"
 import { join } from "path"
+import logger from "morgan"
 
 
 const app = express()
 app.set("view engine", "pug")
 app.set("views", join(__dirname,"views"))
 app.use(express.static(join(__dirname,"static")))
+app.use(logger("dev"))
 app.get("/", (req,res) => res.render("home"))
 const server = app.listen(4000, () => console.log(`✅ server connected : http://localhost:4000 `))
 
 const io = socketIO(server)
+
+let sockets = []
+io.on("connection", (socket)=> {
+    sockets.push(socket.id)
+})
+console.log(sockets)
